8137290de9831b59f90f09cc6d7bddcad0954bcb
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2022-04-28 16:34:13 +0700

    update

--- 002/src/App.js
+++ 002/src/App.js
@@ -1,19 +1,26 @@
 import Navbar from './component/Navbar';
 import GridBox from './component/GridBox';
 import {useState, useEffect} from 'react';
+// import axios from 'axios';
 
 function App() {
 
   const [data,setData] = useState({})
 
-  async function fetchData() {
-      const res = await fetch('http://161.246.5.40:8880/question')
+  const fetchData = async () => {
+    const res = await fetch('http://161.246.5.40:8880/question')
       res.json().then(res => setData(res))
   }
 
+  // false
+  // const fetchData = async () => {
+  //   const res = await axios.get('http://161.246.5.40:8880/question')
+  //   res.then(res => setData(res))
+  // }
+
   useEffect(() => {
     fetchData();
-  })
+  },[1000])
 
   return (
     <div className="container bg-neutral-600 h-screen min-w-full">
